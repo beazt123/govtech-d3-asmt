29015d4793f2c6bbb6dd069ae43ee370bdcf9083
beazt123 <lohzhixuan@live.com>
2021-06-26 17:49:12 +0800

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from lib.utils import serverResponse
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=["GET", "POST"])
+def hello_world():
+    return serverResponse(None, 200, "Yes this endpoint is working")
+    
+
+
+if __name__ == '__main__':
+    # port = int(os.getenv('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port, load_dotenv=".env")
+    app.run()
